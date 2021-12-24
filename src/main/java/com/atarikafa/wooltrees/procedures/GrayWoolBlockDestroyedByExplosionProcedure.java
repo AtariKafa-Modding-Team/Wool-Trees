45954cc318f548341bb69fced61c9abc96a0a9c6
okunamayanad <okunamayanad@gmail.com>
2021-12-25 01:42:54 +0300

    Add Gray GrayWool GrayWoolSapling

---
+++ src/main/java/com/atarikafa/wooltrees/procedures/GrayWoolBlockDestroyedByExplosionProcedure.java
@@ -0,0 +1,55 @@
+package com.atarikafa.wooltrees.procedures;
+
+import net.minecraft.world.World;
+import net.minecraft.world.IWorld;
+import net.minecraft.item.ItemStack;
+import net.minecraft.entity.item.ItemEntity;
+import net.minecraft.block.Blocks;
+
+import java.util.Map;
+
+import com.atarikafa.wooltrees.block.GrayWoolSaplingBlock;
+import com.atarikafa.wooltrees.WoolTreesMod;
+
+public class GrayWoolBlockDestroyedByExplosionProcedure {
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("world") == null) {
+			if (!dependencies.containsKey("world"))
+				WoolTreesMod.LOGGER.warn("Failed to load dependency world for procedure GrayWoolBlockDestroyedByExplosion!");
+			return;
+		}
+		if (dependencies.get("x") == null) {
+			if (!dependencies.containsKey("x"))
+				WoolTreesMod.LOGGER.warn("Failed to load dependency x for procedure GrayWoolBlockDestroyedByExplosion!");
+			return;
+		}
+		if (dependencies.get("y") == null) {
+			if (!dependencies.containsKey("y"))
+				WoolTreesMod.LOGGER.warn("Failed to load dependency y for procedure GrayWoolBlockDestroyedByExplosion!");
+			return;
+		}
+		if (dependencies.get("z") == null) {
+			if (!dependencies.containsKey("z"))
+				WoolTreesMod.LOGGER.warn("Failed to load dependency z for procedure GrayWoolBlockDestroyedByExplosion!");
+			return;
+		}
+		IWorld world = (IWorld) dependencies.get("world");
+		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
+		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
+		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
+		if (0.25 > Math.random()) {
+			if (world instanceof World && !world.isRemote()) {
+				ItemEntity entityToSpawn = new ItemEntity((World) world, x, y, z, new ItemStack(GrayWoolSaplingBlock.block));
+				entityToSpawn.setPickupDelay((int) 0);
+				world.addEntity(entityToSpawn);
+			}
+		} else {
+			if (world instanceof World && !world.isRemote()) {
+				ItemEntity entityToSpawn = new ItemEntity((World) world, x, y, z, new ItemStack(Blocks.GRAY_WOOL));
+				entityToSpawn.setPickupDelay((int) 0);
+				world.addEntity(entityToSpawn);
+			}
+		}
+	}
+}
